bb9865ceb325a36cb7f43f5a15d3bb3f8d874a80
sundersparks <sundersparks@gmail.com>
2017-03-11 16:58:14 -0500

    test

--- TestNG/src/practice/DataProviders.java
+++ TestNG/src/practice/DataProviders.java
@@ -18,9 +18,8 @@ public class DataProviders {
 //Create a test which consumes the Data provided by an object Array 
 @Test(dataProvider = "usercredentials")
 public void LoginTest(String Userid, String Password) { 
-	  System.out.println("This update for git");
 	  System.out.printf("This is User Id : " + Userid + "\n");
 	  System.out.printf("This is password : " + Password+ "\n");
-	  System.out.println("This update for git");
+	  
 }
 }
