3c7765af9c700637e6c3e575b07e89ca7eb163aa
sundersparks <sundersparks@gmail.com>
2017-03-11 07:53:59 -0500

    Test commit to Dataproviders

--- TestNG/src/practice/DataProviders.java
+++ TestNG/src/practice/DataProviders.java
@@ -4,6 +4,7 @@ import org.testng.annotations.Test;
 import org.testng.annotations.DataProvider;
 
 public class DataProviders {	
+	
 // Create a method which provides an Array of object 
 	@DataProvider
 	public static Object[][] usercredentials() { 
@@ -11,13 +12,15 @@ public class DataProviders {
 			{"UID1", "PWD1"},
 			{"UID2", "PWD2"},
 		};
+		
 		}
 	
 //Create a test which consumes the Data provided by an object Array 
 @Test(dataProvider = "usercredentials")
 public void LoginTest(String Userid, String Password) { 
-	  System.out.printf("This is User Id : " + Userid + "\n");
+	  System.out.println("This update for git");
+	  System.out.printf("This is User Id xxxxxxxxxxxxxxxxs: " + Userid + "\n");
 	  System.out.printf("This is password : " + Password+ "\n");
-	
+	  System.out.println("This update for git");
 }
 }
